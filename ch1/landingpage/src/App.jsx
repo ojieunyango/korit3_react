3ed5a8deb4b738eff39992a17f6247c9b384456e
ojieunyango <ojieunyango@gmail.com>
2025-05-21 17:37:48 +0900

    resume

---
+++ ch1/landingpage/src/App.jsx
@@ -0,0 +1,18 @@
+import Header from "./Header"
+import MainComponent from "./MainComponent"
+import Footer from "./Footer"
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <Header></Header>
+      <MainComponent></MainComponent>
+      <Footer></Footer>
+    </>
+  )
+}
+
+export default App
